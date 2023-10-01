c268403dce93385d610d14cf499bcdb58146f8f2
Sadewa <robbybaguss@gmail.com>
2023-10-01 16:50:55 +0700

    added leak canary config

--- app/src/main/java/com/rbs/capstonevalorant/presentation/MainActivity.kt
+++ app/src/main/java/com/rbs/capstonevalorant/presentation/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rbs.capstonevalorant.GlobalSingleton
+import com.rbs.capstonevalorant.GlobalSingletonListener
 import com.rbs.capstonevalorant.databinding.ActivityMainBinding
 import com.rbs.core.domain.model.Agents
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -14,12 +16,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModel()
+    private val listener = Listener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setData()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        GlobalSingleton.register(listener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // we forget to unregister our listener so a reference
+        // of the Singleton to the listener and to the Activity
+        // still exists after the user navigates away fro that
+        // activity
+
+        GlobalSingleton.unregister(listener)
     }
 
     private fun setData() {
@@ -58,5 +77,10 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_AGENTS = "extras"
+    }
+
+    // inner class has implicit reference to enclosing Activity
+    private inner class Listener : GlobalSingletonListener {
+        override fun onEvent() { }
     }
 }
