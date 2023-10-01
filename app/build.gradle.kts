6f3f7d9e7223d13416f42925a0f0a2fc1432a5a9
Sadewa <robbybaguss@gmail.com>
2023-10-01 18:39:12 +0700

    try to fixing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+        task("favorite.exportReleaseConsumerProguardFiles") {
+            dependsOn("favorite.extractProguardFiles")
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
